ae11e579a9939c8ce2c7a43e671a23fa0a8d05f9
maxiplay <maxime.dhalluin@hotmail.fr>
2016-03-15 13:13:31 +0100

    cluster doxn zone changed

--- cluster_down.sh
+++ cluster_down.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 CLUSTER_NAME=${1-imager}
-ZONE=us-central1-f
+ZONE=europe-west1-d
 
 # Delete services
 kubectl delete -f service_jenkins.yaml
